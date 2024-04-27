a5c3160733b90d1bde84e2798f5d4e5a6700eba9
tatianaQ <119733612+tatianaQ@users.noreply.github.com>
2024-04-27 10:45:11 +0300

    Initial commit

---
+++ src/main/java/koschei/config/AppConfig.java
@@ -0,0 +1,27 @@
+package koschei.config;
+
+import koschei.models.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "koschei")
+public class AppConfig {
+
+    @Bean
+    public static Island2 getIsland(Wood3 wood) {
+        return new Island2(wood);
+    }
+
+    @Bean
+    public static Egg6 setEgg(Needle7 needle) {
+        return new Egg6(needle);
+    }
+
+    @Bean
+    public static Needle7 setNeedle(Deth8 deth) {
+        return new Needle7(deth);
+    }
+
+}
---
+++ src/main/java/koschei/models/Deth8.java
@@ -0,0 +1,6 @@
+package koschei.models;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Deth8 {
+}
